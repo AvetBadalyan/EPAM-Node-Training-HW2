3a094cd6fff6c094036c201a410dc0474677d142
AvetBadalyan <avetbadalyan@gmail.com>
2024-08-03 12:22:04 +0400

    task 3 update async task from callbacks to then catch chaining

--- task3_async.js
+++ task3_async.js
@@ -1,4 +1,3 @@
-
 /* Task 3: Synchronous vs. Asynchronous Operations
 **Instructions**:
 1. Write two scripts:
@@ -6,15 +5,17 @@
    - Another that uses asynchronous methods (`fs.readFile`, `fs.writeFile`) to achieve the same functionality.
 2. Compare the scripts and note the differences in their execution. */
 
-const fs = require("fs");
+const fs = require("fs").promises;
 
 const filePath = "asyncFile.txt";
 
-fs.writeFile(filePath, "This is written asynchronously.", "utf8", (err) => {
-  if (err) throw err;
-
-  fs.readFile(filePath, "utf8", (err, data) => {
-    if (err) throw err;
+fs.writeFile(filePath, "This is written asynchronously.", "utf8")
+  .then(() => {
+    return fs.readFile(filePath, "utf8");
+  })
+  .then((data) => {
     console.log(data);
+  })
+  .catch((err) => {
+    console.error(err);
   });
-});
