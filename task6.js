366266eba5e785359ee0b2a2f13aee459b6e7c4e
AvetBadalyan <avetbadalyan@gmail.com>
2024-08-03 12:29:09 +0400

    task 6 update callbacks to promise chaining

--- task6.js
+++ task6.js
@@ -3,14 +3,14 @@
 1. Write a script that attempts to read a file that does not exist.
 2. Implement error handling to gracefully handle the error and print an appropriate message to the console. */
 
-const fs = require("fs");
+const fs = require("fs").promises;
 
 const filePath = "babken.txt";
 
-fs.readFile(filePath, "utf8", (err, data) => {
-  if (err) {
-    console.error(`Error reading file: ${err.message}`);
-  } else {
+fs.readFile(filePath, "utf8")
+  .then((data) => {
     console.log(data);
-  }
-});
+  })
+  .catch((err) => {
+    console.error(`Error reading file: ${err.message}`);
+  });
